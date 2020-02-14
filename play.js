32f89a04a56efbf9339c9aad771e9d032a765f9d
Sujay Das <dasujoy1999@gmail.com>
2020-02-14 17:09:06 +0530

    second commit

--- play.js
+++ play.js
@@ -5,8 +5,8 @@ roundScore = 0;
 activePlayer = 0;
 
 //console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// 
+//
 
 var x = document.querySelector('#score-0').textContent;
 console.log(x);
@@ -27,7 +27,21 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     
-    // 3.
+    // 3. Update the round score IF the rolled number was NOT a 1
+     if (dice !== 1) {
+         roundScore += dice;
+         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+        } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0; 
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
+    }
 
 });
 
